ffc09d61447aeb71ebb13348482a876bd2d5c960
talitaaraujodev <talitacumi.araujo@gmail.com>
2022-09-28 06:35:25 -0300

    add GlobalStyles, theme e HomeScreen

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,3 @@
-import Link from "../src/components/Link";
+import HomeScreen from '../src/screens/HomeScreen';
 
-export default function HomePage() {
-  return (
-    <div>
-      <h1>Alura Cases - Home</h1>
-      <Link href={"/faq"} children={"Ir para FAQ"}/>
-    </div>
-  );
-}
+export default HomeScreen;
